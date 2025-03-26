633936ae96ab269c63505353877d0af3181989ac
nnavnita <navnitanandakumar@gmail.com>
2025-03-26 21:13:49 +1100

    improved freelancers

--- src/interfaces/freelancer.ts
+++ src/interfaces/freelancer.ts
@@ -1,5 +1,34 @@
-export default interface Freelancer {
+export interface Freelancer {
     id: number,
     Name: string,
     Email: string
+};
+
+export interface About {
+    id: number,
+    Title: string,
+    Subtitle: string,
+    Content: string,
+    Images: {
+        [key: string]: any,
+        url: string,
+    }[]
+};
+
+export interface Hero {
+    id: number,
+    Title: string,
+    Subtitle: string,
+    Content: string,
+    Image: {
+        [key: string]: any,
+        url: string,
+    }
+};
+
+export interface FreelancerContent {
+    id: number,
+    Title: string,
+    Subtitle: string,
+    Content: string
 };
